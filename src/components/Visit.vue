761fb6e890b467e9011491a9992364daf2e0ab59
Kamil Łopata <kamillopata@gmail.com>
2018-11-24 19:24:28 +0100

    make search clinic clickable

--- src/components/Visit.vue
+++ src/components/Visit.vue
@@ -14,13 +14,17 @@
       <v-card-text v-if="false">{{ details.examination.description }}</v-card-text>
       <v-card-actions>
         <v-btn>{{ $t('visit.mark_as_dome') }}</v-btn>
-        <v-btn color="primary">{{ $t('visit.find_clinic') }}</v-btn>
+        <v-btn :href="searchClinic" target="_blank" color="primary">
+          {{ $t('visit.find_clinic') }}
+        </v-btn>
       </v-card-actions>
     </v-card>
   </v-flex>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
 export default {
 
   name: 'Visit',
@@ -34,6 +38,15 @@ export default {
     return {
 
     };
+  },
+  computed: {
+    ...mapState([
+      'user',
+    ]),
+    searchClinic() {
+      const query = `${this.details.examination.name}+${this.user.city}`;
+      return `https://google.com/search?q=${encodeURIComponent(query)}`;
+    },
   },
 };
 </script>
--- src/store.js
+++ src/store.js
@@ -5,7 +5,9 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    user: {
+      city: 'Warszawa',
+    },
   },
   mutations: {
 
