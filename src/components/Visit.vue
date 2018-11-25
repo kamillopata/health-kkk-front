7d46c888596535166aff606d36924bfcf3c2a094
Kamil Łopata <kamillopata@gmail.com>
2018-11-25 10:11:18 +0100

    visit card style

--- src/components/Visit.vue
+++ src/components/Visit.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-flex xs10 sm11 my-2>
+  <v-flex xs10 sm11 my-2 @click="$emit('focus')">
     <v-card>
       <v-card-text>
         <div class="headline">
@@ -8,16 +8,27 @@
         <div class="font-weight-light">
           {{ details.duration }}
         </div>
-        <div :class="{ 'text-truncate': !focus }" @click="$emit('focus')">
+        <div :class="{ 'text-truncate': !focus }">
           {{ details.examination.description }}
         </div>
       </v-card-text>
+      <v-card-actions v-show="focus">
+        <v-btn small>
+          <v-icon>event</v-icon>
+          {{ $t('visit.reschedule') }}
+        </v-btn>
+        <v-btn small>
+          <v-icon>done</v-icon>
+          {{ $t('visit.markAsDone') }}
+        </v-btn>
+      </v-card-actions>
       <v-card-actions>
-        <v-btn :icon="true"><v-icon>event</v-icon></v-btn>
-        <v-btn>{{ $t('visit.markAsDone') }}</v-btn>
+        <v-flex xs12 sm4 text-xs-right>
         <v-btn :href="searchClinic" target="_blank" color="primary">
+          <v-icon>local_hospital</v-icon>
           {{ $t('visit.findClinic') }}
         </v-btn>
+        </v-flex>
       </v-card-actions>
     </v-card>
   </v-flex>
@@ -48,7 +59,7 @@ export default {
   },
   computed: {
     searchClinic() {
-      const query = `${this.details.examination.name}+${this.profile.city}`;
+      const query = `${this.details.examination.name} ${this.profile.city}`;
       return `https://google.com/search?q=${encodeURIComponent(query)}`;
     },
   },
